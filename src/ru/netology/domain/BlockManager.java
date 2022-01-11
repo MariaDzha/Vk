ce95a62b1613a1a28c5454b39bb10a6a13d47590
MariaDzha <m_zudina@inbox.ru>
2022-01-11 16:43:37 +0300

    BlockManager modified

--- src/ru/netology/domain/BlockManager.java
+++ src/ru/netology/domain/BlockManager.java
@@ -2,6 +2,46 @@ package ru.netology.domain;
 
 public class BlockManager {
 
+    private class PostID {
+        private int postID;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class OwnerID {
+        private int ownerID;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class FromID {
+        private int fromID;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class CreatedBy {
+        private int createdBy;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
     private class CommunityName {
         private String communityName;
 
@@ -14,6 +54,35 @@ public class BlockManager {
 
     private class PostDate {
         private String postDate;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ReplyOwnerID {
+        private int replyOwnerID;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ReplyPostID {
+        private int replyPostID;
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class FriendsOnly {
+        private boolean friendsOnly;
 
         public String generateBlock() {
 
@@ -45,6 +114,9 @@ public class BlockManager {
 
     private class LikeNumber {
         private int likeNumber;
+        private boolean userLikes;
+        private boolean canLike;
+        private boolean canPublish;
 
         public String generateBlock() {
 
@@ -54,7 +126,11 @@ public class BlockManager {
     }
 
     private class CommentsInfo {
-        private String commentsInfo;
+        private int count;
+        private boolean canPost;
+        private boolean groupsCanPost;
+        private boolean canClose;
+        private boolean canOpen;
 
         public String generateBlock() {
 
@@ -63,8 +139,18 @@ public class BlockManager {
         //+getters/setters
     }
 
-    private class SharePost {
-        private String sharePost;
+    private class Copyright {
+        private Copyright copyright;
+        private int copyrightID;
+        private String copyrightLink;
+        private String copyrightName;
+        private String copyrightType;
+    }
+
+    private class Repost {
+        private Repost repost;
+        private int repostCount;
+        private boolean userReposted;
 
         public String generateBlock() {
 
@@ -75,6 +161,26 @@ public class BlockManager {
 
     private class ViewNumber {
         private int viewNumber;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class PostType {
+        private String postType;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class SignerID {
+        private int signerID;
 
         public String generateBlock() {
 
@@ -125,6 +231,66 @@ public class BlockManager {
 
     private class SaveArticle {
         private String saveArticle;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class CanDelete {
+        private boolean canDelete;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class CanEdit {
+        private boolean canEdit;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class IsPinned {
+        private boolean isPinned;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class MarkedAsAds {
+        private boolean markedAsAds;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class CanPin {
+        private boolean canPin;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class PostponedID {
+        private int postponedID;
 
         public String generateBlock() {
 
