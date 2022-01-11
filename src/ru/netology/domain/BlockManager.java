5456cf77621472e7a9857e3e3781e06785d71403
MariaDzha <m_zudina@inbox.ru>
2022-01-11 10:36:22 +0300

    Version.1

---
+++ src/ru/netology/domain/BlockManager.java
@@ -0,0 +1,135 @@
+package ru.netology.domain;
+
+public class BlockManager {
+
+    private class CommunityName {
+        private String communityName;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class PostDate {
+        private String postDate;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class PostAction {
+        private String claim;
+        private String save;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class PostText {
+        private String postText;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class LikeNumber {
+        private int likeNumber;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class CommentsInfo {
+        private String commentsInfo;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class SharePost {
+        private String sharePost;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ViewNumber {
+        private int viewNumber;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ArticleName {
+        private String articleName;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ArticleSource {
+        private String articleSource;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ReadArticle {
+        private String articleUrl;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class ArticleImage {
+        private String imageUrl;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+
+    private class SaveArticle {
+        private String saveArticle;
+
+        public String generateBlock() {
+
+            return null;
+        }
+        //+getters/setters
+    }
+}
---
+++ src/ru/netology/domain/PostManager.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class PostManager {
+    public BlockManager blockManager;
+
+    public String generateBlock() {
+        return null;
+    }
+}
